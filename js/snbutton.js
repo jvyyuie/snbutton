8509ff2607aec97a259d3619ff0797e411877545
Shang Ning <jvyyuie@gmail.com>
2017-08-08 17:55:10 +0800

    add new features

--- js/snbutton.js
+++ js/snbutton.js
@@ -1,12 +1,29 @@
 /*
- * A tiny jQuery plugin that could control the button's state
+ * A tiny jQuery plugin that could control button's state
  */
 var SNButton = {
-	init: function(e) {
-		var snnode = "#"+$("#"+e).data("snnode");
-		$("#"+e).attr("disabled", ""==$(snnode).val());
-		$(snnode).on('input propertychange', function() {
-			$("#"+e).attr("disabled", ""==$(snnode).val());
+	init: function(e, params) {
+		var button = "#"+e;
+		var input = "#"+jQuery(button).data("snnode");
+
+		if(params)
+		{
+			var enabletext = params.enabletext ? params.enabletext : $(button).text();
+			var disabletext = params.disabletext ? params.disabletext : $(button).text();
+		}
+		jQuery(button).attr("disabled", ""==jQuery(input).val());
+		jQuery(button).text(disabletext);
+		jQuery(input).on('input propertychange', function() {
+		if(jQuery(input).val()=="")
+		{
+			jQuery(button).attr("disabled", true);
+			jQuery(button).text(disabletext);
+		}
+		else
+		{
+			jQuery(button).attr("disabled", false);
+			jQuery(button).text(enabletext);
+		}
 		});
 	}
 }

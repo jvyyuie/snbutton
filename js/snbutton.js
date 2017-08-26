d99be84026d8dd485f526ef4a3ae37629e7dca48
Shang Ning <jvyyuie@gmail.com>
2017-08-27 03:53:38 +0800

    support new feature

--- js/snbutton.js
+++ js/snbutton.js
@@ -1,4 +1,5 @@
 /*
+ * Version 0.2.0
  * A tiny jQuery plugin that could control button's state
  */
 var SNButton = {
@@ -10,20 +11,74 @@ var SNButton = {
 		{
 			var enabletext = params.enabletext ? params.enabletext : $(button).text();
 			var disabletext = params.disabletext ? params.disabletext : $(button).text();
+			var fields = params.fields ? params.fields : null;
 		}
-		jQuery(button).attr("disabled", ""==jQuery(input).val());
-		jQuery(button).text(disabletext);
-		jQuery(input).on('input propertychange', function() {
-		if(jQuery(input).val()=="")
+		if(fields)
 		{
-			jQuery(button).attr("disabled", true);
-			jQuery(button).text(disabletext);
+			input = Array();
+			$.each(fields, function(index, value, array) {
+					input.push("#"+value);
+					});
+		}
+
+		jQuery(button).attr("disabled", SNButton.allhastext(input));
+		jQuery(button).text(disabletext);
+		if(Array.isArray(input))
+		{
+			$.each(input, function(index, value, array) {
+				jQuery(value).on('input propertychange', function() {
+					if(SNButton.allhastext(input))
+					{
+						jQuery(button).attr("disabled", true);
+						jQuery(button).text(disabletext);
+					}
+					else
+					{
+						jQuery(button).attr("disabled", false);
+						jQuery(button).text(enabletext);
+					}
+				});
+			});
 		}
 		else
 		{
-			jQuery(button).attr("disabled", false);
-			jQuery(button).text(enabletext);
+			jQuery(input).on('input propertychange', function() {
+				if(SNButton.allhastext(input))
+				{
+					jQuery(button).attr("disabled", true);
+					jQuery(button).text(disabletext);
+				}
+				else
+				{
+					jQuery(button).attr("disabled", false);
+					jQuery(button).text(enabletext);
+				}
+			});
 		}
-		});
+	},
+	allhastext: function(sdf) {
+		console.log(sdf);
+		if(Array.isArray(sdf))
+		{
+			var ret = false;
+			$.each(sdf, function(index, value, array) {
+				if(jQuery(value).val()=="")
+				{
+					ret = true;
+				}
+			});
+			return ret;
+		}
+		else
+		{
+			if(jQuery(sdf).val()=="")
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
 	}
 }
